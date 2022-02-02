5adead8caa7a391ec0220f8e553086fe3d5d17c2
lemwish <mcdonald.am97@gmail.com>
2022-02-02 18:35:58 -0500

    Initialize Terraform env

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
+  tenant_id         = "${env.ARM_TENANT_ID}"
+  client_id         = "${env.ARM_CLIENT_ID}"
+  client_secret     = "${env.ARM_CLIENT_SECRET}"
+}
